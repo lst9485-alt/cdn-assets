cedcb4af54d050b751da32126699f9669b210e2a
lst9485 <lst9485@gmail.com>
2026-04-06 16:19:26 +0900

    feat: 사업현황 v2 — 세로 퍼널(매출→풀링) + 탭(퍼널/매출) + 콘텐츠 키/풀링 분리

--- sidebar.js
+++ sidebar.js
@@ -32,7 +32,6 @@
     + '<a href="' + root + 'dashboard/#todo" class="sb-sub">투두리스트</a>'
     + '<a href="' + root + 'dashboard/#projects" class="sb-sub">프로젝트</a>'
     + '<a href="' + root + 'dashboard/#okr" class="sb-sub">OKR·루틴</a>'
-    + '<a href="' + root + 'dashboard/sales.html" class="sb-sub">' + ic.money + ' 매출</a>'
     + '<a href="' + root + 'dashboard/business.html" class="sb-sub">' + ic.dash + ' 사업현황</a>'
     + '<div class="sb-cat">프로세스</div>'
     + link(root + 'process/youtube.html', ic.yt, '유튜브', 'youtube')
