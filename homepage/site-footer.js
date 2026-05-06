a89b1f0d104c0f245d4777a7fa8c7d80b080562d
lst9485 <lst9485@gmail.com>
2026-05-06 22:18:01 +0900

    Fix legal wording issues

--- homepage/site-footer.js
+++ homepage/site-footer.js
@@ -36,7 +36,7 @@
       '      <div style="color:#8a8a8a;font-size:13px;line-height:2.15;margin-top:18px">',
       '        <p style="margin:0">(주)우리동네사람들 | 대표자 : 홍윤지 | 소재지 : 당산로 92, 301-이3호(당산동1가, 호서빌딩)</p>',
       '        <p style="margin:0">사업자 등록번호 : 386-86-03832 | 통신판매신고번호 : 제 2026-서울영등포-0991 호</p>',
-      '        <p style="margin:0">개인정보관리책임자 : 홍윤지 | 호스팅제공자 : (주)아임웹</p>',
+      '        <p style="margin:0">개인정보 보호책임자 : 홍윤지 | 호스팅제공자 : (주)아임웹</p>',
       '        <p style="margin:0">대표번호 070-4517-9400</p>',
       '        <p style="margin:0">문의 : contact@ourdongne.com</p>',
       '        <p style="margin:6px 0 0">Ⓒ2026 주식회사 우리동네사람들. All rights reserved.</p>',
