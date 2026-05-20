1976fc6e48132eaace628e8fbc24b69707de0e44
lst9485 <lst9485@gmail.com>
2026-05-20 19:37:53 +0900

    Bump homepage shell cache version

--- homepage/homepage-shell.js
+++ homepage/homepage-shell.js
@@ -1,7 +1,7 @@
 (function () {
   var script = document.currentScript;
   var base = script ? new URL('.', script.src).href : './';
-  var version = '20260506d';
+  var version = '20260520a';
 
   function getFlag(name, defaultValue) {
     if (!script) return defaultValue;
