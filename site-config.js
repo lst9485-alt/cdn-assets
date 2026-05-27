301d13f7f3a374e485421ef64e3012d703a63fd9
lst9485 <lst9485@gmail.com>
2026-05-27 21:34:09 +0900

    Make coaching hero override resilient

--- site-config.js
+++ site-config.js
@@ -172,7 +172,7 @@
             hubVisible: true,
             groupLabel: '상품',
             imwebCode: 'homepage/coaching-all-v3-imweb-copy.html',
-            dateSuffix: '2'
+            dateSuffix: '3'
           },
           {
             id: 'main-page',
