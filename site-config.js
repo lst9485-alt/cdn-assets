dbf10e8533e077454bf15cda245617c138d86726
lst9485 <lst9485@gmail.com>
2026-06-23 16:45:37 +0900

    fix(homepage): 모바일 계산기 좌우폭 확대 + 복리/단리 토글 줄바꿈 수정

--- site-config.js
+++ site-config.js
@@ -395,7 +395,7 @@
             activeMatch: ['retire-column-final.html'],
             hubVisible: true,
             imwebCode: 'homepage/retire-column-imweb-copy.html',
-            dateSuffix: '1'
+            dateSuffix: '2'
           }
         ]
       },
