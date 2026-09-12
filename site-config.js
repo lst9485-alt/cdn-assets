a16371c7063b2717004fabb96ad6a90039ba0dbe
lst9485 <lst9485@gmail.com>
2026-09-12 22:29:10 +0900

    Revert "chore: 코칭 B 마감 배지 9월,10월 → 10월,11월"
    This reverts commit 1c0e4e9bb372369502fea06ee01e9eb4c993d3db.

--- site-config.js
+++ site-config.js
@@ -174,7 +174,7 @@
             hubVisible: true,
             groupLabel: '코칭',
             imwebCode: 'homepage/coaching-all-v3-imweb-copy.html',
-            dateSuffix: '1'
+            dateSuffix: '3'
           },
           {
             id: 'main-page',
